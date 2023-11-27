f2d4a3bd0668633d7893328b71533b08526509f5
Velpula,SravanKumar <sravan_velpula@optum.com>
2023-11-27 17:21:21 +0530

    Complete JSX elements and React styling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("root"));
+function App() {
+    let name = "John";
+    let date = new Date().getDate();
+    let month = new Date().getMonth();
+    let year = new Date().getFullYear();
+    return <div>
+            <h1 className="heading">Hello {name}</h1>
+            <p>Its {date}/{month}/{year}</p>
+           </div>;
+}
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(<App />);
