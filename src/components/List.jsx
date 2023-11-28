686eb865aa038475c897db002f7cdcd5ffd3f182
Velpula,SravanKumar <sravan_velpula@optum.com>
2023-11-28 11:41:47 +0530

    Complete React Components

---
+++ src/components/List.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function List() {
+    return <ul>
+        <li>I am Robo</li>
+        <li>Speed 1 Tera Hertz</li>
+        <li>Memory 1 Zeta Byte</li>
+        </ul>;
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,33 @@
-// Complete react inline Styling and conditional styling
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-var customStyles = {
-    color : "red"
-};
-
-function App() { 
-
-    let today = new Date();
-    let t = today.getHours();
-    let greet;
-    if(t>=0 && t<=12) greet="Good Morning";
-    else if(t>=12 && t<=18) {greet="Good Afternoon"; customStyles.color="green";}
-    else if(t>=18 && t<=23) {greet="Good Night"; customStyles.color="blue";}
-    return <div>
-        <h1 className="greetHeading" style={customStyles}>{greet}</h1>
-    </div>
-};
+import App from "./components/App";
 
 var root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<App/>);
+
+// Complete react inline Styling and conditional styling
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+
+// var customStyles = {
+//     color : "red"
+// };
+
+// function App() { 
+
+//     let today = new Date();
+//     let t = today.getHours();
+//     let greet;
+//     if(t>=0 && t<=12) greet="Good Morning";
+//     else if(t>=12 && t<=18) {greet="Good Afternoon"; customStyles.color="green";}
+//     else if(t>=18 && t<=23) {greet="Good Night"; customStyles.color="blue";}
+//     return <div>
+//         <h1 className="greetHeading" style={customStyles}>{greet}</h1>
+//     </div>
+// };
+
+// var root = createRoot(document.getElementById("root"));
+// root.render(<App />);
 
 // complete JSX elements and React styling
 // import React from "react";
