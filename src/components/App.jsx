f04a5d9bc2c97b93dc19fda4e7489d92842b3d40
Velpula,SravanKumar <sravan_velpula@optum.com>
2023-11-28 11:48:47 +0530

    Complete Practicing react components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
-import Heading from "./Heading";
-import List from "./List";
+function App() { 
+    let customStyles = {
+        color : "red"
+    };
 
-function App() {
+    let today = new Date();
+    let t = today.getHours();
+    let greet;
+    if(t>=0 && t<=12) greet="Good Morning";
+    else if(t>=12 && t<=18) {greet="Good Afternoon"; customStyles.color="green";}
+    else if(t>=18 && t<=23) {greet="Good Night"; customStyles.color="blue";}
     return <div>
-        <Heading/>
-        <List/>
-        </div>;
-}
+        <h1 className="greetHeading" style={customStyles}>{greet}</h1>
+    </div>
+};
 
 export default App;
+
+// import Heading from "./Heading";
+// import List from "./List";
+
+// function App() {
+//     return <div>
+//         <Heading/>
+//         <List/>
+//         </div>;
+// }
+
+// export default App;
+
