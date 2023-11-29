5b9595c6f5391cb00ff91cbd5ce713fd91d74983
Velpula,SravanKumar <sravan_velpula@optum.com>
2023-11-29 18:44:05 +0530

    Complete React Props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,49 @@
-function App() { 
-    let customStyles = {
-        color : "red"
-    };
+import Card from "./Card";
+import contacts from "../contacts";
 
-    let today = new Date();
-    let t = today.getHours();
-    let greet;
-    if(t>=0 && t<=12) greet="Good Morning";
-    else if(t>=12 && t<=18) {greet="Good Afternoon"; customStyles.color="green";}
-    else if(t>=18 && t<=23) {greet="Good Night"; customStyles.color="blue";}
+function App() {
     return <div>
-        <h1 className="greetHeading" style={customStyles}>{greet}</h1>
-    </div>
-};
+        <heading className="heading">React Props</heading>
+        <Card 
+            name={contacts[0].name}
+            imgsrc={contacts[0].imgURL}
+            tel={contacts[0].phone}
+            email={contacts[0].email}
+        />
+        <Card 
+            name={contacts[1].name}
+            imgsrc={contacts[1].imgURL}
+            tel={contacts[1].phone}
+            email={contacts[1].email}
+        />
+        <Card 
+            name={contacts[2].name}
+            imgsrc={contacts[2].imgURL}
+            tel={contacts[2].phone}
+            email={contacts[2].email}
+        />
+    </div>;
+}
 
 export default App;
+
+// function App() { 
+//     let customStyles = {
+//         color : "red"
+//     };
+
+//     let today = new Date();
+//     let t = today.getHours();
+//     let greet;
+//     if(t>=0 && t<=12) greet="Good Morning";
+//     else if(t>=12 && t<=18) {greet="Good Afternoon"; customStyles.color="green";}
+//     else if(t>=18 && t<=23) {greet="Good Night"; customStyles.color="blue";}
+//     return <div>
+//         <h1 className="greetHeading" style={customStyles}>{greet}</h1>
+//     </div>
+// };
+
+// export default App;
 
 // import Heading from "./Heading";
 // import List from "./List";
---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+    {
+      name: "Lorem Ipsum",
+      imgURL:
+        "https://picsum.photos/seed/picsum/200",
+      phone: "1234567890",
+      email: "LoremIpsum@email.com"
+    },
+    {
+      name: "lorem ipsum",
+      imgURL:
+        "https://picsum.photos/200?grayscale",
+      phone: "0987654321",
+      email: "loremipsum@ipsum.com"
+    },
+    {
+      name: "Chuck Norris",
+      imgURL:
+        "https://picsum.photos/200",
+      phone: "0987612345",
+      email: "ipsumlorem@email.com"
+    }
+  ];
+
+  export default contacts;
